7d554c30549c977c74c8acc435c2395eaeff339b
ironiron <bull31@vp.pl>
2019-06-15 20:45:40 +0200

    added watchdog ISR and software spi...testing

--- tester serw.c
+++ tester serw.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * tester_serw.c
  *
  * Created: 2015-01-10 17:53:29
@@ -19,104 +19,151 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/wdt.h>
 
 #define button (1<<PB1)
 #define output (1<<PB0)
 #define potenciometer (1<<PB4)
 #define set_output PORTB|=(1<<PB0)
 #define clear_output PORTB&=~(1<<PB0)
+#define TCNT0_BASE 105 // tick resolution for manipulating PWM lenght is 150, thus 255-150=105.
+
+//SPI related
+#define SET_SS PORTB|=(1<<PB5)
+#define SET_CLOCK PORTB|=(1<<PB2)
+#define SET_MOSI PORTB|=(1<<PB3)
+
+#define RESET_SS PORTB&=~(1<<PB5)
+#define RESET_CLOCK PORTB&=~(1<<PB2)
+#define RESET_MOSI PORTB&=~(1<<PB3)
+
 
 volatile uint8_t adc=0;
 volatile uint8_t remember=0;
-volatile uint8_t ticks=0;
+volatile uint16_t ticks=0;
 volatile uint8_t position=0;
+volatile uint8_t overflows_left=0;
+
 enum current_mode {normal, neutral, oscillating} current_mode;
 enum direction {left, right} direction;
-
-ISR(TIM0_OVF_vect)
-{
-	TCNT0=162; /*interrupt every 19,8 ms (f_cpu= 1200000 Hz)*/
+enum state {waiting, PWM, PWM_overflows} state;
 	
+void _send_SPI(uint8_t data)
+{
+	//CPOL=0
+	//CPHA=0
+	RESET_CLOCK;
+	RESET_MOSI;
+	RESET_SS;
+	for(uint8_t i=0;i<8;i++)
+	{
+		if((data<<i) & 0x0f)
+		{
+			SET_MOSI;
+		}
+		else
+		{
+			RESET_MOSI;
+		}
+		SET_CLOCK;
+		_delay_us(10);
+		RESET_CLOCK;
+		_delay_us(10);
+	}
+	SET_SS;
+	
+}
+
+void calculate_ticks(void)
+{
 	switch (current_mode)
 	{
 		case normal:
-		if(adc==0)
-		{
-			adc=1;
-		}
-		ticks=adc;
-		set_output;
-		/*asm loop (10us)*/
-		asm(
-		"1: dec %0" "\n\t"
+		ticks=adc*3/2+75;// 1 adc=10ms; add 75 so begins counting fom 0,5ms.
 		
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"brne 1b"
-		: "=r" (ticks)
-		: "0" (ticks)
-		);
-		_delay_us(490);
-		clear_output;
 		break;
 		
 		case neutral:
-		set_output;
-		_delay_us(500);
-		_delay_us(500);
-		_delay_us(500);/*delay =1.5ms (max delay of this func. can be only 637 us at 1,2MHz)*/
-		clear_output;
+		ticks=225;//one tick is 6,(6)us, so 225ticks=1,5ms
+		overflows_left=0;
 		break;
 		
 		case oscillating:
-		ticks=position;
-		set_output;
-		/*asm loop (10us)*/
-		asm(
-		"1: dec %0" "\n\t"
-
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"nop" "\n\t"
-		"nop" "\n\t"
-		"nop" "\n\t"
-		
-		"brne 1b"
-		: "=r" (ticks)
-		: "0" (ticks)
-		);
-		_delay_us(490);
-		clear_output;
 		adc=adc/20;
 		if(direction==left)
 		{
 			if(position<adc)position=0;// if position-adc<0
 			else position=position-adc;
 		}
-		else 
+		else
 		{
 			if(position+adc>200)position=200;
 			else position=position+adc;
 		}
 		if(position==200)direction=left;
 		if(position==0)direction=right;
-		if(position==0)position=1;;
+		if(position==0)position=1;
+		ticks=position*3/2+75;// 1 adc=10ms
+		
+		break;
+	}
+			if(ticks>0xff)
+			{
+				overflows_left=ticks/0xff;
+				ticks=ticks%255;
+			}
+	
+}
+	
+void set_timer_generating(void)
+{
+	TCCR0B = (1<<CS01); /*up-counting prescaler 8, interrupt enable*/
+}
+
+void set_timer_waiting(void)
+{
+	TCCR0B = (1<<CS02); /*up-counting prescaler 256, interrupt enable*/
+	TCNT0=162; /*interrupt every 19,8 ms (f_cpu= 1200000 Hz)*/
+}
+
+ISR(TIM0_OVF_vect)
+{
+	
+	switch(state)
+	{
+		case waiting:
+			set_timer_generating();
+			if (overflows_left!=0)//check if more than 0xff ticks are required
+			{
+				state=PWM_overflows;
+				overflows_left--;
+				//TCNT0 already set to 0 by default.
+			}
+			else
+			{
+				state=PWM;
+				TCNT0=0xff-ticks;
+			}
+			set_output;
+		break;
+		
+		case PWM:
+			clear_output;
+			state=waiting;
+			set_timer_waiting();
+			calculate_ticks();
+		break;
+		
+		case PWM_overflows:
+			if (overflows_left!=0)
+			{
+				overflows_left--;
+			}
+			else
+			{
+				TCNT0=0xff-ticks;
+				state=PWM;
+			}
 		break;
 	}
 }
@@ -141,18 +188,36 @@ ISR(INT0_vect)
 	sei();
 }
 
+ISR(WDT_vect)
+{
+	_send_SPI(0x78);
+	_send_SPI(0x20);
+}
+
 int main(void)
 {
-    DDRB |= output;
+    	DDRB |= output;
 	PORTB |=button;
+
+	DDRB |= (1<<PB2)|(1<<PB3)|(1<<PB5);
 	
 	GIMSK |=(1<<INT0); /*interrupt on low level*/
 	
-	TCCR0B |= (1<<CS02); /*up-counting prescaler 256, interrupt enable*/
+	set_timer_waiting();
+	state=waiting;
+	
 	TIMSK0 |= (1<<TOIE0);
 	
 	ADCSRA |=(1<<ADEN) | (1<<ADPS2);		 /*prescaler 16 */
 	ADMUX |=(1<<MUX1); /*input- PB4*/
+	
+	wdt_enable(WDTO_120MS);
+	WDTCR |= (1<<WDTIE);
+//WDTCR |= (0<<WDP3) | (1<<WDP2) | (0<<WDP1) | (1<<WDP0);
+
+// Set watchdog timer in interrupt mode
+// WDTCR |= (1<<WDTIE);
+// WDTCR |= (0<<WDE);
 	sei();
 	
     while(1)
@@ -160,9 +225,9 @@ int main(void)
 		ADCSRA |=(1<<ADSC);
 		while(ADCSRA & (1<<ADSC))
 		adc=ADC/5;	/*result- over 200*/
-		if(adc>201)
+		if(adc>200)
 		{
-			adc=201;
+			adc=200;
 		}
 		if(remember+30<adc || remember>adc+30)
 		{
